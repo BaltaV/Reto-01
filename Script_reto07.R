2c0e33379e9fd88f0ec7de41892b52b553922ad0
BaltaV <baltasar.vez@gmail.com>
2022-08-11 07:12:18 +0000

    completo

--- Script_reto07.R
+++ Script_reto07.R
@@ -1,1 +1,27 @@
 # reto de la sesion 07
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'CountryLanguage')
+
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+names(DataDB)
+
+SP <- DataDB %>% filter(Language == "Spanish")
+
+
+
+SP %>% ggplot(aes( x = CountryCode, y=Percentage, fill = IsOfficial )) + 
+  geom_bin2d() +
+  coord_flip()
